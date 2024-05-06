093ca248554bf65e46c7d56838e213c30336c0e3
Admin <ecrai30@wgu.edu>
2024-05-06 11:52:42 -0700

    Updated CreateAccountController

--- src/main/java/com/example/demo/controllers/CreateAccountController.java
+++ src/main/java/com/example/demo/controllers/CreateAccountController.java
@@ -1,11 +1,18 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class CreateAccountController {
 
     @GetMapping("/createAccount")
     public String displayCreateAccount(){
         return"/CreateAccount";
     }
+/**
+    @GetMapping("/createAccountSuccess")
+    public String displayCreateAccountSuccess(){
+        return"/CreateAccountSuccess";
+    }
+ **/
 }
