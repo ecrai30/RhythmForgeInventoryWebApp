f69159a1ac328638374e125ec2998cd80b23f68e
Ezekiel Craig <ecrai30@wgu.edu>
2024-05-08 23:17:55 -0500

    Updated file

--- src/main/java/com/example/demo/controllers/CreateAccountController.java
+++ src/main/java/com/example/demo/controllers/CreateAccountController.java
@@ -1,13 +1,43 @@
 package com.example.demo.controllers;
 
+import com.example.demo.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class CreateAccountController {
 
     @GetMapping("/createAccount")
     public String displayCreateAccount(){
         return"/CreateAccount";
+    }
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody String addNewUser (@RequestParam String name
+            , @RequestParam String email, @RequestParam String password, @RequestParam String username)  {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        n.setPassword(password);
+        n.setUsername(username);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
     }
 /**
     @GetMapping("/createAccountSuccess")
